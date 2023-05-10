290860e0635f807f5ae854427553dab86624faa9
ahorn275 <125283940+ahorn275@users.noreply.github.com>
2023-05-10 00:29:50 -0400

    Update WineNode.java
    Add index attribute and header

--- WineNode.java
+++ WineNode.java
@@ -1,3 +1,14 @@
+// **********************************************************************************
+// Title: Wine Node
+// Author: Autumn Horn
+// Course Section: CMIS202-ONL1 (Seidel) Spring 2023
+// File: WineNode.java
+// Description: Creates a Serializable WineNode for use in WineList and WineTree.
+//    The wine variable stores a Wine object. The left and right variables represent
+//    the left and right children of the current node in a binary search tree, and the
+//    next and prev variables represent the next and previous nodes in a linked list.
+//    The index variable represents the position of the current node in the list.
+// **********************************************************************************
 import java.io.Serializable;
 
 public class WineNode<Wine> implements Serializable {
@@ -6,8 +17,10 @@ public class WineNode<Wine> implements Serializable {
     protected WineNode<Wine> right;
     protected WineNode<Wine> next;
     protected WineNode<Wine> prev;
+    protected int index;
 
     public WineNode(Wine wine) {
         this.wine = wine;
+        this.index = index;
     }
 }
